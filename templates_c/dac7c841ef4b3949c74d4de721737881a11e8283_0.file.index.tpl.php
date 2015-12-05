eb3c29f9e3b5030e56d1015a23228a3656e5e48f
jim <jsdalton@cox.net>
2015-12-05 15:58:52 -0800

    modified index.tpl

--- templates_c/dac7c841ef4b3949c74d4de721737881a11e8283_0.file.index.tpl.php
+++ templates_c/dac7c841ef4b3949c74d4de721737881a11e8283_0.file.index.tpl.php
@@ -1,7 +1,7 @@
-<?php /* Smarty version 3.1.27, created on 2015-12-04 17:50:06
+<?php /* Smarty version 3.1.27, created on 2015-12-05 15:51:28
          compiled from "/var/www/test/index.tpl" */ ?>
 <?php
-/*%%SmartyHeaderCode:1158143399566242ce575608_56833641%%*/
+/*%%SmartyHeaderCode:63887296256637880a37fb3_60990179%%*/
 if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
@@ -9,25 +9,46 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'dac7c841ef4b3949c74d4de721737881a11e8283' => 
     array (
       0 => '/var/www/test/index.tpl',
-      1 => 1449280200,
+      1 => 1449359484,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '1158143399566242ce575608_56833641',
+  'nocache_hash' => '63887296256637880a37fb3_60990179',
   'variables' => 
   array (
     'Contacts' => 0,
   ),
   'has_nocache_code' => false,
   'version' => '3.1.27',
-  'unifunc' => 'content_566242ce5d51f2_66468193',
+  'unifunc' => 'content_56637880a899d5_53715792',
 ),false);
 /*/%%SmartyHeaderCode%%*/
-if ($_valid && !is_callable('content_566242ce5d51f2_66468193')) {
-function content_566242ce5d51f2_66468193 ($_smarty_tpl) {
+if ($_valid && !is_callable('content_56637880a899d5_53715792')) {
+function content_56637880a899d5_53715792 ($_smarty_tpl) {
 
-$_smarty_tpl->properties['nocache_hash'] = '1158143399566242ce575608_56833641';
+$_smarty_tpl->properties['nocache_hash'] = '63887296256637880a37fb3_60990179';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Example of Bootstrap 3 Vertical Form Layout</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
+<?php echo '<script'; ?>
+ src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"><?php echo '</script'; ?>
+> 
+<style type="text/css">
+    .bs-example{
+    	margin: 20px;
+    }
+</style>
+</head>
+<body>
+
 
 
 <?php echo "BILL TO:";?>
@@ -53,6 +74,8 @@ echo ",";
 echo $_smarty_tpl->tpl_vars['Contacts']->value['shipstate'];
 echo $_smarty_tpl->tpl_vars['Contacts']->value['shipzip'];?>
 <br /><br/>
+</body>
+</html>
 
 <?php }
 }
